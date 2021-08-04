33e55c25394b19f8453a4128bff01a9045c01cbe
Anh Dang <anhcdang@gmail.com>
2021-08-04 12:45:13 -0700

    create #index in workouts

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,4 +1,9 @@
 class WorkoutsController < ApplicationController
+    def index
+        workouts = Workout.all
+        render json: workouts
+    end
+
     def create
         workout = Workout.create(workout_params)
     end
