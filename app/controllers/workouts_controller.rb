db7ddff031e6468c7944e1ea869a21afd99369a1
Anh Dang <anhcdang@gmail.com>
2021-08-04 11:45:33 -0700

    add #create to workouts

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,2 +1,11 @@
 class WorkoutsController < ApplicationController
+    def create
+        workout = Workout.create(workout_params)
+    end
+
+    private
+
+    def workout_params
+        params.permit(:name)
+    end
 end
