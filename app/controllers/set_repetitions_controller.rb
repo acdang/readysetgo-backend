9c648cd79c08debde85a2667ad575b2b9496e7a0
Anh Dang <anhcdang@gmail.com>
2021-08-07 11:07:52 -0700

    update to allow array of objects params

--- app/controllers/set_repetitions_controller.rb
+++ app/controllers/set_repetitions_controller.rb
@@ -5,13 +5,21 @@ class SetRepetitionsController < ApplicationController
     end
 
     def create
-        set_repetition = SetRepetition.create(set_repetition_params)
-        render json: set_repetition
+        # set_repetition = SetRepetition.create(set_repetition_params)
+        # render json: set_repetition
+
+        # loop through array of SetRepetition objects and create SetRepetition
+        set_repetitions = set_repetition_params[:object_array].map do | set_repetition_object |
+            SetRepetition.create(set_repetition_object)
+        end
+        # debugger
+        render json: set_repetitions
     end
 
     private
 
     def set_repetition_params
-        params.permit(:block_id, :exercise_set_id)
+        # params.permit(:block_id, :exercise_set_id)
+        params.require(:set_repetition).permit(object_array: [:block_id, :exercise_set_id])
     end
 end
