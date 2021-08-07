21bd4772c671b3bdb08aa6ef5a2591be42e44e57
Anh Dang <anhcdang@gmail.com>
2021-08-07 09:46:19 -0700

    update to pass in array of objects

--- app/controllers/exercise_sets_controller.rb
+++ app/controllers/exercise_sets_controller.rb
@@ -5,13 +5,21 @@ class ExerciseSetsController < ApplicationController
     end
 
     def create
-        exercise_set = ExerciseSet.create(exercise_set_params)
-        render json: exercise_set
+        # loop through array of ExerciseSet objects and create ExerciseSet
+        exercise_sets = exercise_set_params[:object_array].map do | exercise_object |
+            ExerciseSet.create(exercise_object)
+        end
+        # debugger
+        # exercise_set = ExerciseSet.create(exercise_set_params)
+        render json: exercise_sets
     end
 
     private
 
     def exercise_set_params
-        params.permit(:exercise_id, :exercise_rep_num, :rest_time, :weight, :active_time) # :exercise_id, :set_rep
+        # params.permit(:exercise_id, :exercise_rep_num, :rest_time, :weight, :active_time) # :exercise_id, :set_rep
+        # params.permit(:exercise_set_objects_array)
+        # params.permit(exercise_set_objects_array: [:exercise_id, :exercise_rep_num, :rest_time, :weight, :active_time])
+        params.require(:exercise_set).permit(object_array: [:exercise_id, :exercise_rep_num, :rest_time, :weight, :active_time])
     end
 end
